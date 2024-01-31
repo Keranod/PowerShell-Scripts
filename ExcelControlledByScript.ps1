ca32bd6b2f73c7479c0a3c668a02928a1442d867
Keranod <70093810+Keranod@users.noreply.github.com>
2024-01-31 11:12:49 +0000

    Add files via upload

---
+++ ExcelControlledByScript.ps1
@@ -0,0 +1,24 @@
+﻿<# Create and use with a shortcut with target set to:
+ C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass "Path\ExcelControlledByScript.ps1" -spreadsheetPath "spreadsheet file path"
+#>
+
+param(
+    #Path to the spreadsheet that is going to be controlled
+    [Parameter(Mandatory=$true)]
+    [String]$spreadsheetPath
+)
+
+$excel = New-Object -comobject Excel.Application
+$excel.Visible = $true
+$workbook = $excel.Workbooks.Open($spreadsheetPath)
+
+while ($true) {
+    Start-Sleep -Seconds 30
+    try {
+        $workbook.Save()
+    }
+    catch {break}
+}
+
+#Clears variables and modules
+Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *
